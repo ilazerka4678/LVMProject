02cadd87d2b17f9ad61a08beea8bbb59063a5afc
BT_4N2_02 <ilazerka4678@bths.edu>
2023-03-23 12:29:21 -0400

    main updated

--- src/hardDrive.java
+++ src/hardDrive.java
@@ -1,10 +1,12 @@
 
 public class hardDrive extends LVM{
     private int size;
+    private String sizeString;
 
-    public hardDrive(String n, int s){
+    public hardDrive(String n, int s, String size){
         super(n);
-        size = s;
+        this.size = s;
+        sizeString = this.size + size;
     }
     public int getSize(){
         return size;
--- src/main.java
+++ src/main.java
@@ -1,11 +1,12 @@
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         LVM control = new LVM("control");
-        hardDrive sda = new hardDrive("sda", 100);
-        control.addHardDrive(sda);
-        hardDrive sdb = new hardDrive("sdb", 200);
-        control.addHardDrive(sdb);
-        System.out.println(control.listDrives());
+        String input = "";
+        while (!(input.equals("exit"))){
+            input = s.nextLine();
 
+        }
     }
 }
